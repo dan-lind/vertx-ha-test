c9ca46be2f67b947b346eb7332190d3fc807cfdf
Marc Engström <marc.engstrom@omegapoint.se>
2015-09-05 13:04:55 +0200

    Message for receiving protobuf message

--- labb/notis-webservice/src/main/java/com/omegapoint/ws/RestServiceImpl.java
+++ labb/notis-webservice/src/main/java/com/omegapoint/ws/RestServiceImpl.java
@@ -1,35 +1,33 @@
 package com.omegapoint.ws;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.omegapoint.protobuf.NotisProtos;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class RestServiceImpl extends AbstractVerticle implements RestService {
 
+    private final int PORT = 8080;
     @Override
     public void start() {
 
         Router router = Router.router(vertx);
 
         router.route().handler(BodyHandler.create());
-        router.get("/push/gcm/:message").handler(new Handler<RoutingContext>() {
-            @Override
-            public void handle(RoutingContext routingContext) {
-                RestServiceImpl.this.sendPushToGCM(routingContext);
-            }
-        });
-        router.get("/push/apns/:message").handler(new Handler<RoutingContext>() {
-            @Override
-            public void handle(RoutingContext routingContext) {
-                RestServiceImpl.this.sendPushToGCM(routingContext);
-            }
-        });
+        router.get("/push/gcm/:message").handler(routingContext -> RestServiceImpl.this.sendPushToGCM(routingContext));
+        router.put("/push/apns/:message").handler(routingContext -> RestServiceImpl.this.sendPushToAPNS(routingContext));
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+        System.out.println("Listening on " + PORT);
+        vertx.createHttpServer().requestHandler(router::accept).listen(PORT);
     }
 
     private void sendError(int statusCode, HttpServerResponse response) {
@@ -40,17 +38,43 @@ public class RestServiceImpl extends AbstractVerticle implements RestService {
     public void sendPushToGCM(RoutingContext routingContext) {
         String message = routingContext.request().getParam("message");
         HttpServerResponse response = routingContext.response();
-        response.putHeader("content-type", "text/html").end("Cooool");
+        response.putHeader("content-type", "text/html").end("You wrote: "+message);
 
-        System.out.println("Message: "+message);
+        System.out.println("Message on GCM: "+message);
+
+        EventBus eb = vertx.eventBus();
+        MessageConsumer<String> consumer = eb.consumer("news.uk.sport");
+        consumer.handler(msg -> System.out.println("I have received a message: " + msg.body()));
     }
 
     @Override
     public void sendPushToAPNS(RoutingContext routingContext) {
-        String message = routingContext.request().getParam("message");
-        HttpServerResponse response = routingContext.response();
-        response.putHeader("content-type", "text/html").end("Cooool");
 
-        System.out.println("Message: "+message);
+        HttpServerRequest request = routingContext.request();
+        String contentType = request.headers().get("Content-Type");
+        System.out.println("Content-Type = " + contentType);
+
+        Buffer data = routingContext.getBody();
+        byte[] contents = data.getBytes();
+        try {
+            NotisProtos.Notis notis = NotisProtos.Notis.parseFrom(contents);
+
+            System.out.println("Protobuf message length = " + data.length() + "\nProtobuf message: " + notis.getMsg());
+
+            String message = routingContext.request().getParam("message");
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("Content-Length", Integer.toString(("You wrote: ".getBytes().length + message.getBytes().length)));
+            response.putHeader("Content-Type", "text/html");
+            response.end("You wrote: " + message);
+
+            System.out.println("Message on APNS: " + message);
+
+            EventBus eb = vertx.eventBus();
+
+            //eb.publish("news.uk.sport", "Yay! Someone kicked a ball");
+            eb.send("news.uk.sport", "Yay! Someone kicked a ball");
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
     }
 }
