732297b8f422b0f37084afb9ac919e36e78260ae
Marc Engström <marc.engstrom@omegapoint.se>
2015-09-07 09:01:10 +0200

    Notification handlers

---
+++ labb/notis-domain/src/main/java/com/omegapoint/bid/notification/GCMNotificationHandler.java
@@ -0,0 +1,9 @@
+package com.omegapoint.bid.notification;
+
+public class GCMNotificationHandler implements NotificationHandler {
+
+    @Override
+    public void push(String deviceId, String message) {
+
+    }
+}
