63b510b5ab6ec21599decbc36d30c272b039e98d
Marc Engström <marc.engstrom@omegapoint.se>
2015-09-08 15:53:29 +0200

    apns module

---
+++ labb/notis-verticles/notis-notification-handlers/notis-apns/src/main/java/com/omegapoint/notification/handlers/apns/ApnsReceiver.java
@@ -0,0 +1,21 @@
+package com.omegapoint.notification.handlers.apns;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class ApnsReceiver extends AbstractVerticle {
+
+
+
+  @Override
+  public void start() throws Exception {
+    EventBus eb = vertx.eventBus();
+
+    eb.consumer("apns-feed", message -> System.out.println("Received APNs-message: " + message.body()));
+
+    System.out.println("Ready!");
+  }
+}
