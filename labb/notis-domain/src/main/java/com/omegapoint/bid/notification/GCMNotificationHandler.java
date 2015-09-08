68c1cf16d1068fe5c5d1911b59ab2e526c01245f
Daniel Lindberg <daniel.lindberg@omegapoint.se>
2015-09-08 16:49:56 +0200

    Finished restructuring

--- labb/notis-domain/src/main/java/com/omegapoint/bid/notification/GCMNotificationHandler.java
+++
@@ -1,9 +0,0 @@
-package com.omegapoint.bid.notification;
-
-public class GCMNotificationHandler implements NotificationHandler {
-
-    @Override
-    public void push(String deviceId, String message) {
-
-    }
-}
--- labb/notis-verticles/notis-notification-handlers/notis-gcm/src/main/java/com/omegapoint/notification/handlers/gcm/GcmReceiver.java
+++ labb/notis-verticles/notis-notification-handlers/notis-gcm/src/main/java/com/omegapoint/notification/handlers/gcm/GcmReceiver.java
@@ -1,8 +1,15 @@
 package com.omegapoint.notification.handlers.gcm;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
 public class GcmReceiver extends AbstractVerticle {
 
 
@@ -12,12 +19,42 @@ public class GcmReceiver extends AbstractVerticle {
 
     EventBus eb = vertx.eventBus();
 
+    MongoClient mongoClient = initDbClient();
+
     //eb.consumer("gcm-feed", message -> System.out.println("Received GCM-message: " + message.body()));
      eb.consumer("gcm-feed", message ->
-             message.reply("Hello")
-     );
+             mongoClient.insert("notification",  new JsonObject().put("title", message.body()), res -> {
+                 if (res.succeeded()) {
 
+                     System.out.println("Saved to DB: " + message);
 
-    System.out.println("GCM Verticle Ready!");
+                 } else {
+
+                     System.out.println("Failed to save to DB: " + message);
+                 }
+             })
+      );
+
+    System.out.println("Ready!");
   }
+
+    private MongoClient initDbClient() {
+        JsonObject config = Vertx.currentContext().config();
+
+        String uri = config.getString("mongo_uri");
+        if (uri == null) {
+            uri = "mongodb://localhost:27017";
+        }
+        String db = config.getString("mongo_db");
+        if (db == null) {
+            db = "test";
+        }
+
+        JsonObject mongoconfig = new JsonObject()
+                .put("connection_string", uri)
+                .put("db_name", db);
+
+
+        return MongoClient.createShared(vertx, mongoconfig);
+    }
 }
